fc95e136cde31cf42f94a83c831d8c66a6d1d269
TerryCowboy <terry@ace2z.com>
2023-11-27 20:54:18 -0700

    'Auto_QuickCommit_for_GOMOD_11_27_2023_20_54'

--- Gadgets/Mongolian/Mongolian.go
+++ Gadgets/Mongolian/Mongolian.go
@@ -10,22 +10,20 @@ This depreciates the old tried and true mgo/mango driver
 package CUSTOM_GOMOD
 
 import (
-    "context"
-    "time"
-    "strings"
+	"context"
+	"strings"
+	"time"
 
-   . "github.com/ace2z/GOGO/Gadgets"
+	. "github.com/ace2z/GOGO/Gadgets"
 
-
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "go.mongodb.org/mongo-driver/mongo/readpref"
-    "go.mongodb.org/mongo-driver/bson"    
-
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 /*  REMEMBER!! for GO structs..if you have nested Structs in your struct.. you HAVE TO include the omitempty tag
-type GAME_OBJ struct {	
+type GAME_OBJ struct {
 	SUMMARY 		string	`bson:"SUMMARY"`
 	DATE 			string	`bson:"DATE"`
 
@@ -34,13 +32,15 @@ type GAME_OBJ struct {
 
 	HOME_BOX_INFO	[]BOX_INFO_OBJ	`bson:"HOME_BOX_INFO,omitempty"`
 	AWAY_BOX_INFO	[]BOX_INFO_OBJ	`bson:"AWAY_BOX_INFO,omitempty"`
-		
+
 	DATE_OBJ 		time.Time	`bson:"DATE_OBJ"`
 }
 */
 
+var MONGO_PORT = "27017" // defualt mongo port.. can be overrided if you ever need to for some reason
 
-var MONGO_PORT = "27017"            // defualt mongo port.. can be overrided if you ever need to for some reason
+var MONGO_DBNAME = "" // name of the database to use
+var MONGO_HOST = ""   // hostname of the DB to use
 
 var MONGO_CLIENT *mongo.Client
 var MONGO_CLIENT_OPTIONS *options.ClientOptions
@@ -52,49 +52,49 @@ var MON_PREFIX = " --| "
 var err error
 
 func TestFUNC() {
-    C.Println("MOngolian TESTFUNC")
+	C.Println("MOngolian TESTFUNC")
 }
 
 func MONGO_SHOW_DATABASES() {
 
-    //2. Show Databases
-    Y.Println(MON_PREFIX, "Listing Databases: ")
-    databases, err := MONGO_CLIENT.ListDatabaseNames(MONGO_CONTEXT, bson.M{})
-    if err != nil {
-        M.Println(err.Error())
-        return
-    }
-    SHOW_STRUCT(databases)
+	//2. Show Databases
+	Y.Println(MON_PREFIX, "Listing Databases: ")
+	databases, err := MONGO_CLIENT.ListDatabaseNames(MONGO_CONTEXT, bson.M{})
+	if err != nil {
+		M.Println(err.Error())
+		return
+	}
+	SHOW_STRUCT(databases)
 
 }
 func MONGO_INIT(mongo_host string) {
 
-    var mongo_url = "mongodb://" + mongo_host + ":" + MONGO_PORT
-    
-    Y.Print(MON_PREFIX, "Connecting To MONGO: ")
-    W.Print(mongo_url)
+	var mongo_url = "mongodb://" + mongo_host + ":" + MONGO_PORT
 
-    var MONGO_CONTEXT, MONGO_CANCEL = context.WithTimeout(context.Background(), time.Duration(MONGO_TIMEOUT_SECS) * time.Second )
-    MONGO_CLIENT_OPTIONS = options.Client().ApplyURI(mongo_url)
-    defer MONGO_CANCEL()
+	Y.Print(MON_PREFIX, "Connecting To MONGO: ")
+	W.Print(mongo_url)
 
-    MONGO_CLIENT, err = mongo.Connect(MONGO_CONTEXT, options.Client().ApplyURI(mongo_url))
+	var MONGO_CONTEXT, MONGO_CANCEL = context.WithTimeout(context.Background(), time.Duration(MONGO_TIMEOUT_SECS)*time.Second)
+	MONGO_CLIENT_OPTIONS = options.Client().ApplyURI(mongo_url)
+	defer MONGO_CANCEL()
 
-    if err != nil {
-        M.Println(err.Error())
-        return
-    }
-    G.Println(" Success!")
+	MONGO_CLIENT, err = mongo.Connect(MONGO_CONTEXT, options.Client().ApplyURI(mongo_url))
 
-    Y.Print(MON_PREFIX, "Test Ping of Connection:")
-    err = MONGO_CLIENT.Ping(MONGO_CONTEXT, readpref.Primary())
-    if err != nil {
-        M.Println(err.Error())      
-        return
-    }
-    C.Println(" Ping Works!")
+	if err != nil {
+		M.Println(err.Error())
+		return
+	}
+	G.Println(" Success!")
 
-//    MONGO_SHOW_DATABASES()
+	Y.Print(MON_PREFIX, "Test Ping of Connection:")
+	err = MONGO_CLIENT.Ping(MONGO_CONTEXT, readpref.Primary())
+	if err != nil {
+		M.Println(err.Error())
+		return
+	}
+	C.Println(" Ping Works!")
+
+	// MONGO_SHOW_DATABASES()
 }
 
 /* BROKEN not yet working
@@ -118,108 +118,103 @@ func NEW_COMPOUND_INDEX(indexname string, itype string, unique bool, PARAMS ...s
 }
 */
 
-
-
 var TEMP_INDEXES []string
 
 // Pass INdex Name, the type (asc,desc,text).. and true/false (for unique or not)
 func NEW_INDEX(indexname string, itype string, unique bool) {
 
-    var result = ""
-    if itype == "asc" {
-        result = indexname + "|asc"
+	var result = ""
+	if itype == "asc" {
+		result = indexname + "|asc"
 
-    } else if itype == "desc" {
-        result = indexname + "|desc"
+	} else if itype == "desc" {
+		result = indexname + "|desc"
 
-    } else if itype == "text" {
-        result = indexname + "|text"
-    }
+	} else if itype == "text" {
+		result = indexname + "|text"
+	}
 
-    if unique {
-        result += "|yes"
-    }
+	if unique {
+		result += "|yes"
+	}
 
-    TEMP_INDEXES = append(TEMP_INDEXES, result)
+	TEMP_INDEXES = append(TEMP_INDEXES, result)
 }
-
 
 func CREATE_DATABASE(dbname string, collname string) {
 
-    Y.Println("")
-    Y.Print(MON_PREFIX, "Creating DATABASE: ")
-    W.Println(dbname)
+	Y.Println("")
+	Y.Print(MON_PREFIX, "Creating DATABASE: ")
+	W.Println(dbname)
 
-    //3. Create a database:
-    var MONGO_DB_OBJ = MONGO_CLIENT.Database(dbname)
-    var MONGO_COLL_OBJ = MONGO_DB_OBJ.Collection(collname)
+	//3. Create a database:
+	var MONGO_DB_OBJ = MONGO_CLIENT.Database(dbname)
+	var MONGO_COLL_OBJ = MONGO_DB_OBJ.Collection(collname)
 
-     // Now create the indexes (the ones that are SINGLE indexes)
-    for _, x := range TEMP_INDEXES {
+	// Now create the indexes (the ones that are SINGLE indexes)
+	for _, x := range TEMP_INDEXES {
 
-        msplit := strings.Split(x, "|") 
+		msplit := strings.Split(x, "|")
 
-        index_field_name := msplit[0]
-        field_type := msplit[1]
-        
+		index_field_name := msplit[0]
+		field_type := msplit[1]
 
-        uni_flag := false 
-        if len(msplit) == 3 {
-            unique := msplit[2]
-            if unique == "yes" || unique == "true" || strings.Contains(unique, "unique") {
-                uni_flag = true
-            }
-        }
+		uni_flag := false
+		if len(msplit) == 3 {
+			unique := msplit[2]
+			if unique == "yes" || unique == "true" || strings.Contains(unique, "unique") {
+				uni_flag = true
+			}
+		}
 
-        var indexName = ""
-        var err error
+		var indexName = ""
+		var err error
 
-        if field_type == "asc" {
-            indexName, err = MONGO_COLL_OBJ.Indexes().CreateOne(
-                context.Background(),
-                mongo.IndexModel{
-                        Keys: bson.M{
-                            index_field_name: 1,
-                        },
-                        Options: options.Index().SetUnique(uni_flag),
-                },
-            )
+		if field_type == "asc" {
+			indexName, err = MONGO_COLL_OBJ.Indexes().CreateOne(
+				context.Background(),
+				mongo.IndexModel{
+					Keys: bson.M{
+						index_field_name: 1,
+					},
+					Options: options.Index().SetUnique(uni_flag),
+				},
+			)
 
-        } else if field_type == "desc" {
-            indexName, err = MONGO_COLL_OBJ.Indexes().CreateOne(
-                context.Background(),
-                mongo.IndexModel{
-                        Keys: bson.M{
-                            index_field_name: -1,
-                        },
-                        Options: options.Index().SetUnique(uni_flag),
-                },
-            )
-        } else if field_type == "text" {
-            indexName, err = MONGO_COLL_OBJ.Indexes().CreateOne(
-                context.Background(),
-                mongo.IndexModel{
-                        Keys: bson.M{
-                            index_field_name: "text",
-                        },
-                        Options: options.Index().SetUnique(uni_flag),
-                },
-            )
-        }
-    
-        if err != nil {
-            M.Println(" Index Creation ERROR: ", err.Error())
-            return
-        }
-    
-        Y.Print(MON_PREFIX, "Created INDEX: ")
-        G.Print(indexName)
-        C.Println("", field_type)
+		} else if field_type == "desc" {
+			indexName, err = MONGO_COLL_OBJ.Indexes().CreateOne(
+				context.Background(),
+				mongo.IndexModel{
+					Keys: bson.M{
+						index_field_name: -1,
+					},
+					Options: options.Index().SetUnique(uni_flag),
+				},
+			)
+		} else if field_type == "text" {
+			indexName, err = MONGO_COLL_OBJ.Indexes().CreateOne(
+				context.Background(),
+				mongo.IndexModel{
+					Keys: bson.M{
+						index_field_name: "text",
+					},
+					Options: options.Index().SetUnique(uni_flag),
+				},
+			)
+		}
 
-    } //end of for
+		if err != nil {
+			M.Println(" Index Creation ERROR: ", err.Error())
+			return
+		}
 
-    // Purge the index array  .. ALWAYS DO THIS!!!
-    TEMP_INDEXES = []string{}
-    
-    
+		Y.Print(MON_PREFIX, "Created INDEX: ")
+		G.Print(indexName)
+		C.Println("", field_type)
+
+	} //end of for
+
+	// Purge the index array  .. ALWAYS DO THIS!!!
+	TEMP_INDEXES = []string{}
+
 }
--- Gadgets/Mongolian/go.mod
+++ Gadgets/Mongolian/go.mod
@@ -1,22 +1,22 @@
 module github.com/ace2z/GOGO/Gadgets/Mongolian
 
-go 1.20
+go 1.21.4
 
 require (
-	github.com/ace2z/GOGO/Gadgets v0.0.0-20230629124046-51c0a4f58a22
-	go.mongodb.org/mongo-driver v1.12.0
+	github.com/ace2z/GOGO/Gadgets v0.0.0-20231127022913-03af07f64256
+	go.mongodb.org/mongo-driver v1.13.0
 )
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/briandowns/spinner v1.23.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/fatih/color v1.15.0 // indirect
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
@@ -24,7 +24,7 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/term v0.1.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
