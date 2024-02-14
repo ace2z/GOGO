8ef93659484b9353395ef713f8054e3a5ebe8e8f
TerryCowboy <terry@ace2z.com>
2024-02-14 16:39:45 -0700

    Recent Local updates

--- Gadgets/Scraper/Scrape_FROM_FILE.go
+++ Gadgets/Scraper/Scrape_FROM_FILE.go
@@ -1,15 +1,14 @@
 package CUSTOM_GO_MODULE
 
-import (    
+import (
 	"os"
-	//. "local/_CORE"    
-    //. "local/_DATABASE"
-    . "github.com/ace2z/GOGO/Gadgets"
-    //. "github.com/ace2z/GOGO/Gadgets/Mongolian"
+	//. "local/CORE"
+	//. "local/_DATABASE"
+	. "github.com/ace2z/GOGO/Gadgets"
+	//. "github.com/ace2z/GOGO/Gadgets/Mongolian"
 
-    "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
-
 
 func SCRAPE_from_FILE(filename string) (bool, *goquery.Document) {
 	// Read from FILE
@@ -21,9 +20,8 @@ func SCRAPE_from_FILE(filename string) (bool, *goquery.Document) {
 		M.Println("Error in SCRAPE_from_FILE: ")
 		W.Println(err)
 		return false, doc
-	}	
+	}
 	defer f.Close()
-
 
 	doc, err = goquery.NewDocumentFromReader(f)
 	if err != nil {
