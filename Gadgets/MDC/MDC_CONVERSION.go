2f140a0747037df6716369027ef01736acc90a47
TerryCowboy <terry@ace2z.com>
2024-03-17 22:22:59 -0600

    'Auto_QuickCommit_for_GOMOD_03_17_2024_22_22'

--- Gadgets/MDC/MDC_CONVERSION.go
+++ Gadgets/MDC/MDC_CONVERSION.go
@@ -4,67 +4,66 @@
 NOTE: For Functions or Variables to be globally availble. The MUST start with a capital letter.
 	  (This is a GO Thing)
 
-	
+
 	Aug 28, 2021    v1.23   - Initial Rollout
 
 */
 
 package GOGO_MDC
 
-
 import (
 
 	// = = = = = Native Libraries
-		"time"
-		//"math/rand"
+	"time"
+	//"math/rand"
 
 	// = = = = = CUSTOM Libraries
-		. "github.com/ace2z/GOGO/Gadgets"
-		//. "github.com/ace2z/GOGO/Gadgets/StringOPS"
-
-
+	. "github.com/ace2z/GOGO/Gadgets"
+	//. "github.com/ace2z/GOGO/Gadgets/StringOPS"
 	// = = = = = 3rd Party Libraries
-
 )
 
 // Use ful Time zone locations
 var LOCAL_Location_OBJ, _ = time.LoadLocation("Local")
 var EST_Location_OBJ, _ = time.LoadLocation("America/New_York")
-var CST_Location_OBJ, _ = time.LoadLocation("America/Chicago")			// aka CST	}
-var MST_Location_OBJ, _ = time.LoadLocation("America/Denver") 		// MDT / Mountain Standard
-var PST_Location_OBJ, _ = time.LoadLocation("America/Los_Angeles")		// aka PST
-var UTC_Location_OBJ, _ = time.LoadLocation("UTC") 
-
+var CST_Location_OBJ, _ = time.LoadLocation("America/Chicago")     // aka CST	}
+var MST_Location_OBJ, _ = time.LoadLocation("America/Denver")      // MDT / Mountain Standard
+var PST_Location_OBJ, _ = time.LoadLocation("America/Los_Angeles") // aka PST
+var UTC_Location_OBJ, _ = time.LoadLocation("UTC")
 
 /*
-	Converts the following 
-		- a STRING date (in the proper format) to a time.Time DATE_OBJ
-		- a DATE_OBJ into a 'pretty date'
-		- an EPOCH ..  into a pretty date.. and time.Time Date_OBJ
-		  (specify this with int(EPOCH_wasint64)  .. this is because Go doesnt recognize int64 as a param explicitly
+Converts the following
+  - a STRING date (in the proper format) to a time.Time DATE_OBJ
+  - a DATE_OBJ into a 'pretty date'
+  - an EPOCH ..  into a pretty date.. and time.Time Date_OBJ
+    (specify this with int(EPOCH_wasint64)  .. this is because Go doesnt recognize int64 as a param explicitly
 
-	TZONE: Specify cst, est, mdt or pst if you need to override the timezone format returned
+TZONE: Specify cst, est, mdt or pst if you need to override the timezone format returned
 
-	STRING format for the Date must be in one of the following or you will error:
-		- MM-DD-YYYY
-		- YYYY-MM-DD		(ISO / British format)
-		
-		- MM/DD/YYYY
-		- YYYY/MM/DD
+STRING format for the Date must be in one of the following or you will error:
 
-		Also accepts TIME.. Which must be apppended as:  (24 hour format only supported)
+  - MM-DD-YYYY
+  - 2024-03-15 18:30:00		( ISO Full British Format )
+  - YYYY-MM-DD		(ISO / British format)
 
-		- XXXXX_18:05
-		- XXXXX@18:05	
+  - MM/DD/YYYY
+
+  - YYYY/MM/DD
+
+    Also accepts TIME.. Which must be apppended as:  (24 hour format only supported)
+
+  - XXXXX_18:05
+
+  - XXXXX@18:05
 
 
-	Final param is for FORMAT specifiy: basic, simple, full, nano, british, justtime, justdate, timestamp
-	(this uses SHOW_PRETTY_DATE )
-	You can also modify format by adding: 
-	_noday   (ie, basic_noweek) - Prevents the weekday info from showing
-	_nozone   		- prevents the timezone info from showing
-	_reset_time 		- For situations where you want to omit the HH:MM cause you dont need it...resets time to 00:00
 
+Final param is for FORMAT specifiy: basic, simple, full, nano, british, justtime, justdate, timestamp
+(this uses SHOW_PRETTY_DATE )
+You can also modify format by adding:
+_noday   (ie, basic_noweek) - Prevents the weekday info from showing
+_nozone   		- prevents the timezone info from showing
+_reset_time 		- For situations where you want to omit the HH:MM cause you dont need it...resets time to 00:00
 */
 func CONVERT_DATE(ALL_PARAMS ...interface{}) (string, string, time.Time) {
 
@@ -73,12 +72,12 @@ func CONVERT_DATE(ALL_PARAMS ...interface{}) (string, string, time.Time) {
 	var EPOCH_input int
 
 	var output_FORMAT = ""
-	var TIMEZONE_OBJ, _ = time.LoadLocation("Local")		// we always default to local
+	var TIMEZONE_OBJ, _ = time.LoadLocation("Local") // we always default to local
 
 	var need_DATE_convert = false
 	var need_STRING_convert = false
 	var need_EPOCH_convert = false
-	
+
 	//1. First parameter is always the input date in the proper format
 	for n, param := range ALL_PARAMS {
 		string_val, IS_STRING := param.(string)
@@ -104,19 +103,20 @@ func CONVERT_DATE(ALL_PARAMS ...interface{}) (string, string, time.Time) {
 				Y.Print("Input was: ")
 				W.Println(param)
 				DO_EXIT()
-			}			
+			}
 			continue
 		}
 
 		//2. All if est/cst/mdt is passed.. get THAT timezone object
 		// Get the timezone object.. assuming est/cst etc is passed
 		if IS_STRING {
-			var is_supported = false
-			var tmp_obj *time.Location
-			is_supported, tmp_obj = get_TZ_OBJECT(string_val)
+			is_supported, tmp_obj := get_TZ_OBJECT(string_val)
 			if is_supported {
 				TIMEZONE_OBJ = tmp_obj
 				continue
+			} else {
+				M.Println("Invalid TZ Sent to Convert Date")
+				Y.Println(string_val)
 			}
 		}
 
@@ -124,13 +124,12 @@ func CONVERT_DATE(ALL_PARAMS ...interface{}) (string, string, time.Time) {
 		if IS_STRING {
 			output_FORMAT = string_val
 		}
-	} //end of for	
-
+	} //end of for
 
 	/*
-	= = = 
-	= = =	This is handling for the DEFAULT two date formats
-	= = =
+		= = =
+		= = =	This is handling for the DEFAULT two date formats
+		= = =
 	*/
 
 	//4. If a STRING was passed..w e convert it to a DATE_OBJ
@@ -158,34 +157,34 @@ func CONVERT_DATE(ALL_PARAMS ...interface{}) (string, string, time.Time) {
 		var num_Hour = pmap["hour"].(int)
 		var num_Min = pmap["min"].(int)
 		var num_Sec = pmap["sec"].(int)
-		monthObj := time.Month(num_Mon)		
+		monthObj := time.Month(num_Mon)
 		date_OBJ := time.Date(num_Year, monthObj, num_Day, num_Hour, num_Min, num_Sec, 0, dummyLOC)
 		date_OBJ = date_OBJ.In(TIMEZONE_OBJ)
-	
+
 		//12. Now pass to show_Pretty_Date with the output format if specified
 		OUTPUT, weekday := SHOW_PRETTY_DATE(date_OBJ, output_FORMAT)
 
-		return OUTPUT, weekday, date_OBJ		
+		return OUTPUT, weekday, date_OBJ
 
-	//5. If were converting a date_OBJ to a string
+		//5. If were converting a date_OBJ to a string
 	} else if need_DATE_convert {
 
 		OUTPUT, weekday := SHOW_PRETTY_DATE(DATE_input, output_FORMAT)
 		return OUTPUT, weekday, DATE_input
 
-	//6. If they passed an EPOCH Int64... 
+		//6. If they passed an EPOCH Int64...
 	} else if need_EPOCH_convert {
 
 		s64 := int64(EPOCH_input)
-		date_OBJ := time.Unix(s64, 0)		
+		date_OBJ := time.Unix(s64, 0)
 		date_OBJ = date_OBJ.In(TIMEZONE_OBJ)
-		
+
 		OUTPUT, weekday := SHOW_PRETTY_DATE(date_OBJ, output_FORMAT)
 		return OUTPUT, weekday, date_OBJ
-	
+
 	} else {
 		M.Print("*** INVALID Date Format Specified!!! ")
-		Y.Println(" to CONVERT_DATE")			
+		Y.Println(" to CONVERT_DATE")
 		DO_EXIT()
 	}
 
@@ -193,17 +192,9 @@ func CONVERT_DATE(ALL_PARAMS ...interface{}) (string, string, time.Time) {
 	return "", "", time.Time{}
 } //end of func
 
-
-
-
-
-
 // Kept here as filler/example.. anything you put in this function will start when the module is imported
 func init() {
 
 	//1. Startup Stuff (init the command line params etc) . We need these Time ZONE Objects
-
-
-
 
 } // end of main
--- Gadgets/MDC/MDC_CONV_helpers.go
+++ Gadgets/MDC/MDC_CONV_helpers.go
@@ -4,76 +4,69 @@
 NOTE: For Functions or Variables to be globally availble. The MUST start with a capital letter.
 	  (This is a GO Thing)
 
-	
+
 	Aug 28, 2021    v1.23   - Initial Rollout
 
 */
 
 package GOGO_MDC
 
-
 import (
 
 	// = = = = = Native Libraries
-		"time"
-		"strings"
-		//"math/rand"
+	"strings"
+	"time"
+
+	//"math/rand"
 
 	// = = = = = CUSTOM Libraries
 
 	. "github.com/ace2z/GOGO/Gadgets"
-
-
 	// = = = = = 3rd Party Libraries
-
 )
 
-
-
-
 func get_TZ_OBJECT(TZ_to_use string) (bool, *time.Location) {
-	var TIMEZONE_OBJ = time.Local
+	var TIMEZONE_OBJ = UTC_Location_OBJ
 
 	is_valid_input := false
 
 	//2. Time Zone logic
 	if TZ_to_use != "" {
 		switch TZ_to_use {
-			case "est":
-				TIMEZONE_OBJ = EST_Location_OBJ
-				is_valid_input = true
-				break
+		case "est":
+			TIMEZONE_OBJ = EST_Location_OBJ
+			is_valid_input = true
+			break
 
-			case "cst":
-				TIMEZONE_OBJ = CST_Location_OBJ
-				is_valid_input = true
-				break
+		case "cst":
+			TIMEZONE_OBJ = CST_Location_OBJ
+			is_valid_input = true
+			break
 
-			case "mdt":
-				TIMEZONE_OBJ = MST_Location_OBJ
-				is_valid_input = true
-				break
+		case "mdt":
+			TIMEZONE_OBJ = MST_Location_OBJ
+			is_valid_input = true
+			break
 
-			case "mst":
-				TIMEZONE_OBJ = MST_Location_OBJ
-				is_valid_input = true
-				break				
+		case "mst":
+			TIMEZONE_OBJ = MST_Location_OBJ
+			is_valid_input = true
+			break
 
-			case "pst":
-				TIMEZONE_OBJ = PST_Location_OBJ
-				is_valid_input = true
-				break
+		case "pst":
+			TIMEZONE_OBJ = PST_Location_OBJ
+			is_valid_input = true
+			break
 
-			case "utc":
-				TIMEZONE_OBJ = UTC_Location_OBJ
-				is_valid_input = true
-				break
+		case "local":
+			TIMEZONE_OBJ = LOCAL_Location_OBJ
+			is_valid_input = true
+			break
 		}
 	}
 
 	return is_valid_input, TIMEZONE_OBJ
 }
-
 
 func FIX_if_needed(input string) string {
 
@@ -89,16 +82,14 @@ func FIX_if_needed(input string) string {
 		return fixed
 	}
 
-
 	// otherwise return
 	return input
 }
 
-
-
 func vsplit_delims(r rune) bool {
-    return r == '_' || r == ','
+	return r == '_' || r == ','
 }
+
 // checks for a verbose Date that looks like this: June_1,_2023
 // May 28, 2023
 func check_for_VERBOSE_DATE(input string) (bool, string, string, string, string, string, string) {
@@ -125,12 +116,12 @@ func check_for_VERBOSE_DATE(input string) (bool, string, string, string, string,
 }
 
 func conv_SPLIT_delims(r rune) bool {
-    return r == '@' || r == ':' || r == '_' || r == '-' || r == '/'
+	return r == '@' || r == ':' || r == '_' || r == '-' || r == '/' || r == ' '
 }
 func have_SUPPORTED_DEFAULT_FORMAT(inputDate string) (bool, map[string]interface{}) {
 
 	var resMAP map[string]interface{}
-	
+
 	sMon := ""
 	sDay := ""
 	sYear := ""
@@ -138,17 +129,15 @@ func have_SUPPORTED_DEFAULT_FORMAT(inputDate string) (bool, map[string]interface
 	sMin := "0"
 	sSec := "0"
 
-
-
 	// First check to see if we have a date in verbose format: June 1, 2023
 	is_VERBOSE := false
-	is_VERBOSE, sMon, sDay, sYear, sHour, sMin, sSec  = check_for_VERBOSE_DATE(inputDate)
+	is_VERBOSE, sMon, sDay, sYear, sHour, sMin, sSec = check_for_VERBOSE_DATE(inputDate)
 
 	//1. If it is NOT verbose...and is a MM/DD/YYYY   (or YYYY/MM/DD) format.. proceed with split
 	if is_VERBOSE == false {
 
-		//1. Do a split.. 
-		sd := strings.FieldsFunc(inputDate, conv_SPLIT_delims)	
+		//1. Do a split..
+		sd := strings.FieldsFunc(inputDate, conv_SPLIT_delims)
 		//error handling check to see if we have enough items
 		if len(sd) < 3 {
 			return false, resMAP
@@ -158,17 +147,23 @@ func have_SUPPORTED_DEFAULT_FORMAT(inputDate string) (bool, map[string]interface
 		part_a := sd[0]
 		part_b := sd[1]
 		part_c := sd[2]
-		
+
 		//2. Check for british format first
-		if len(part_a) == 4 && ( len(part_b) == 2 || len(part_b) == 1 ) {
+		if len(part_a) == 4 && (len(part_b) == 2 || len(part_b) == 1) {
 
 			sYear = strings.TrimSpace(part_a)
 
 			sMon = FIX_if_needed(part_b)
 			sDay = FIX_if_needed(part_c)
 
-		//3. else check for normal xx/yy/zzzz  format
-		} else if ( len(part_a) == 2 || len(part_a) == 1)  && (len(part_b) == 2 || len(part_b) == 1) && ( len(part_c) == 4 || len(part_c) == 2 ) {
+			// Now lets see if we have a full british format date with time
+			if len(sd) == 6 {
+				sHour = FIX_if_needed(sd[3])
+				sMin = FIX_if_needed(sd[4])
+			}
+
+			//3. else check for normal xx/yy/zzzz  format
+		} else if (len(part_a) == 2 || len(part_a) == 1) && (len(part_b) == 2 || len(part_b) == 1) && (len(part_c) == 4 || len(part_c) == 2) {
 			sMon = FIX_if_needed(part_a)
 			sDay = FIX_if_needed(part_b)
 			sYear = part_c
@@ -182,7 +177,6 @@ func have_SUPPORTED_DEFAULT_FORMAT(inputDate string) (bool, map[string]interface
 			return false, resMAP
 		}
 
-		
 		//4. Now determine if we have a TIME appended.. via the :
 		if strings.Contains(inputDate, ":") {
 			sHour = strings.TrimSpace(sd[3])
@@ -196,21 +190,22 @@ func have_SUPPORTED_DEFAULT_FORMAT(inputDate string) (bool, map[string]interface
 	}
 
 	//5. Finally make the map
-	resMAP = map[string]interface{} {
-		"month" : sMon,
-		"day" : sDay,
-		"year" : sYear,
-		"hour": sHour,
-		"min": sMin,
-		"sec": sSec,
-	}	
+	resMAP = map[string]interface{}{
+		"month": sMon,
+		"day":   sDay,
+		"year":  sYear,
+		"hour":  sHour,
+		"min":   sMin,
+		"sec":   sSec,
+	}
 
 	return true, resMAP
 }
 
 func epoch_SPLIT_delims(r rune) bool {
-    return r == ' ' || r == ':' || r == '-'
+	return r == ' ' || r == ':' || r == '-'
 }
+
 // EPOCH format looks like this: must confrom EPOCH_Time_CONVERT
 // Mon Jan 02-01-2006 15:04:05
 func have_EPOCH_FORMAT(input string) (bool, map[string]interface{}) {
@@ -228,13 +223,13 @@ func have_EPOCH_FORMAT(input string) (bool, map[string]interface{}) {
 	}
 	if valid_score == 3 {
 		sd := strings.FieldsFunc(input, epoch_SPLIT_delims)
-		resMAP = map[string]interface{} {
-			"month" : strings.TrimSpace(sd[2]),
-			"day" : strings.TrimSpace(sd[3]),
-			"year" : strings.TrimSpace(sd[4]),
-			"hour": strings.TrimSpace(sd[5]),
-			"min": strings.TrimSpace(sd[6]),
-			"sec": strings.TrimSpace(sd[7]),
+		resMAP = map[string]interface{}{
+			"month": strings.TrimSpace(sd[2]),
+			"day":   strings.TrimSpace(sd[3]),
+			"year":  strings.TrimSpace(sd[4]),
+			"hour":  strings.TrimSpace(sd[5]),
+			"min":   strings.TrimSpace(sd[6]),
+			"sec":   strings.TrimSpace(sd[7]),
 		}
 	} else {
 		return false, resMAP
