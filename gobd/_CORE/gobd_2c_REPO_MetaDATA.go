e988fac66c06e29cd95be5a62851dec5d1ea70de
TerryCowboy <terry@ace2z.com>
2024-11-24 23:38:38 -0700

    quick commit, updated.. Sunday - Nov 24, 2024 @ 23:38

--- gobd/_CORE/gobd_2c_REPO_MetaDATA.go
+++ gobd/_CORE/gobd_2c_REPO_MetaDATA.go
@@ -2,8 +2,9 @@ package _CORE
 
 import (
 	"os"
-	"strings"
 	"path/filepath"
+	"strings"
+
 	. "github.com/ace2z/GOGO/Gadgets"
 )
 
@@ -11,17 +12,16 @@ var MOD_LOCAL_PATH = ""
 var MOD_LOCAL_BASEDIR = ""
 
 var MOD_DIRPATH = ""
-//var JUST_MOD_PACKAGE_NAME = ""
+
+// var JUST_MOD_PACKAGE_NAME = ""
 var OFFICIAL_MODULE_IMPORT_NAME = "local"
 var REPO_URL = ""
 var PARENT_REPO_NAME = ""
 
 var REPO_LOCAL_ROOT = ""
 
-
-
 func repo_delims(r rune) bool {
-    return r == ' '
+	return r == ' '
 }
 
 func url_delims(r rune) bool {
@@ -29,11 +29,9 @@ func url_delims(r rune) bool {
 }
 func GET_REPO_MetaDATA() {
 	cwd, _ := os.Getwd()
-	
 
 	Y.Println("   -| Determinig REPO Meta Data..")
 
-	
 	result, _ := RUN_COMMAND("git remote show origin 2>&1")
 
 	sd := strings.FieldsFunc(result, repo_delims)
@@ -44,12 +42,12 @@ func GET_REPO_MetaDATA() {
 	tmprd := REPO_URL
 	if strings.Contains(REPO_URL, "@") {
 		rd := strings.FieldsFunc(REPO_URL, url_delims)
-		tmprd = rd[1]		
+		tmprd = rd[1]
 		is_private = true
 	} else {
 		tmprd = strings.TrimPrefix(tmprd, "https://")
 	}
-	PARENT_REPO_NAME = strings.TrimSuffix(tmprd, ".git")		
+	PARENT_REPO_NAME = strings.TrimSuffix(tmprd, ".git")
 
 	res2, _ := RUN_COMMAND("git rev-parse --show-toplevel")
 
@@ -64,20 +62,25 @@ func GET_REPO_MetaDATA() {
 	}
 
 	//2. Need all this stuff as is and in this order.. dont change it
-	MOD_LOCAL_PATH = strings.TrimSuffix(res2, "\n")	
+	MOD_LOCAL_PATH = strings.TrimSuffix(res2, "\n")
 	REPO_LOCAL_ROOT := MOD_LOCAL_PATH
 	MOD_LOCAL_BASEDIR = strings.TrimSuffix(filepath.Base(res2), "\n")
 	msplit := strings.Split(cwd, MOD_LOCAL_BASEDIR)
+
+	Y.Println("msplit is: ")
+	SHOW_STRUCT(msplit)
+
 	MOD_DIRPATH = strings.TrimPrefix(msplit[1], "/")
+
+	C.Println("Now MOD_DIRPATH is: ", MOD_DIRPATH)
 
 	MOD_LOCAL_BASEDIR = MOD_DIRPATH
 
-
 	//3. If we are in same directory as REPO LOCAL Root.. and trying to initalize a module
-	// Note: right now we're only supporting GITHUB ... for Azure, Gitlab and Bitbucket, we have different paths that are 
+	// Note: right now we're only supporting GITHUB ... for Azure, Gitlab and Bitbucket, we have different paths that are
 	// used for the "official module import name".. and need adjust accordingly
 	if INIT_MOD {
-		if REPO_LOCAL_ROOT == cwd  {
+		if REPO_LOCAL_ROOT == cwd {
 			MOD_LOCAL_BASEDIR = cwd
 			OFFICIAL_MODULE_IMPORT_NAME = PARENT_REPO_NAME
 		} else {
@@ -90,40 +93,33 @@ func GET_REPO_MetaDATA() {
 		OFFICIAL_MODULE_IMPORT_NAME = strings.TrimSuffix(OFFICIAL_MODULE_IMPORT_NAME, "/")
 	}
 
-	
-
 	// Gets the module IMPORT name based on the Github service being used
 	//WHAT_GIT_Service_Being_Used()
 
-
 	if VERBOSE_MODE {
-		C.Print(PREFIX, "REPO_URL" + ": ")
+		C.Print(PREFIX, "REPO_URL"+": ")
 		if is_private {
 			M.Print("(PRIVATE!) ")
 		}
-	
+
 		Y.Println(REPO_URL)
-		C.Print(PREFIX, "REPO_LOCAL_ROOT" + ": ")
+		C.Print(PREFIX, "REPO_LOCAL_ROOT"+": ")
 		Y.Println(REPO_LOCAL_ROOT)
-		C.Print(PREFIX, "PARENT_REPO_NAME" + ": ")
+		C.Print(PREFIX, "PARENT_REPO_NAME"+": ")
 		Y.Println(PARENT_REPO_NAME)
-		C.Print(PREFIX, "MOD_LOCAL_BASEDIR" + ": ")
+		C.Print(PREFIX, "MOD_LOCAL_BASEDIR"+": ")
 		Y.Println(MOD_LOCAL_BASEDIR)
 	}
 
 	if OFFICIAL_MODULE_IMPORT_NAME == "local" {
-		C.Print(PREFIX, "Regular GO Program (not module)" + ": ")
+		C.Print(PREFIX, "Regular GO Program (not module)"+": ")
 		G.Println("Yes!")
 	} else {
-		
-		C.Print(PREFIX, "Official MODULE Name" + ": ")
+
+		C.Print(PREFIX, "Official MODULE Name"+": ")
 		G.Println(OFFICIAL_MODULE_IMPORT_NAME)
 	}
 
 	// Debug till we have all this right
 	//DO_EXIT()
 }
-
-	
-
-		
--- gobd/go.mod
+++ gobd/go.mod
@@ -1,17 +1,17 @@
 module local
 
-go 1.22.2
+go 1.23.3
 
-require github.com/ace2z/GOGO/Gadgets v0.0.0-20240415110721-9ca41b787d49
+require github.com/ace2z/GOGO/Gadgets v0.0.0-20240922222729-be32985258ff
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/briandowns/spinner v1.23.0 // indirect
+	github.com/briandowns/spinner v1.23.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/fatih/color v1.16.0 // indirect
+	github.com/fatih/color v1.17.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/term v0.1.0 // indirect
 )
