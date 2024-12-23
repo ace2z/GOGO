b3ff7b4397a117131375dd0dadc1e7841b343c4e
TerryCowboy <terry@ace2z.com>
2024-12-23 15:24:08 -0700

    quick commit, updated.. Monday - Dec 23, 2024 @ 15:24

--- __TOOLS/gobd/_CORE/gobd_2d_FigureOutPaths.go
+++ __TOOLS/gobd/_CORE/gobd_2d_FigureOutPaths.go
@@ -21,18 +21,28 @@ func Determine_PATHS_Engine(PARENT_REPO_NAME string, toplevel string) {
 		return
 	}
 	lastEL := msplit[len(msplit)-1]
-	C.Println("LastELE: ", lastEL)
 
 	//for loop in reverse on cwd output
 	csplit := strings.Split(cwd, "/")
+
+	var OFFICIAL_MOD_PATH = PARENT_REPO_NAME
+
+	var TPATHS []string
 
 	for i := len(csplit) - 1; i >= 0; i-- {
 		tmp := csplit[i]
 		if tmp == lastEL {
 			break
 		}
-		Y.Println(tmp)
+		TPATHS = append(TPATHS, tmp)
 	}
+
+	// Now go through TPATHS
+	for i := len(TPATHS) - 1; i >= 0; i-- {
+		OFFICIAL_MOD_PATH = OFFICIAL_MOD_PATH + "/" + TPATHS[i]
+	}
+
+	G.Println("OFFICIAL_MOD_PATH: ", OFFICIAL_MOD_PATH)
 
 	PressAny()
 
